373ea0d14318c928f67873dee93029399f6c51ac
Mustafa Zırhlı <120317376+Mustafazirhli98@users.noreply.github.com>
2023-11-25 19:31:10 +0300

    Add files via upload
    proje dosyaları yüklendi

---
+++ data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        "city": "İstanbul",
+        "url": "https://c4.wallpaperflare.com/wallpaper/470/124/1006/5bd3ac6f39ff6-wallpaper-preview.jpg",
+        "id": "1"
+    },
+    {
+        "city": "Londra",
+        "url": "https://e1.pxfuel.com/desktop-wallpaper/611/15/desktop-wallpaper-big-ben-london.jpg",
+        "id": "2"
+    },
+    {
+        "city": "Paris",
+        "url": "https://static.trip101.com/paragraph_media/pictures/002/351/453/large/1200px-Paris_75007_Champ-de-Mars_lawn_20170526_Eiffel_Tower__282_29.jpg?1610420875",
+        "id" : "3"
+    },
+    {
+        "city": "Singapur",
+        "url": "https://c4.wallpaperflare.com/wallpaper/312/691/797/singapore-look-at-night-desktop-wallpaper-hd-for-mobile-phones-and-pc-1920%C3%971200-wallpaper-preview.jpg",
+        "id": "4"
+
+    },
+    {
+        "city": "New York",
+        "url": "https://w0.peakpx.com/wallpaper/362/609/HD-wallpaper-new-york-usa-big-city-traffics-crowded-time-square.jpg",
+        "id": "5"
+
+    },
+    {
+        "city": "Hong Kong",
+        "url": "https://www.reedsmith.com/-/media/images/news/2020/hongkong001_461869699_800x400px.jpg?rev=753fddb07dc4489eb4202e34a15e536b",
+        "id": "6"
+
+    },
+]
---
+++ script.js
@@ -0,0 +1,73 @@
+const createCard = () => {
+    data.map(item => {
+        let card = `
+                <div class="card-parent">
+                    <div class="city-card" style ="background-image:url('${item.url}')">
+                    <div class="times" id=${item.id}>
+                    </div>
+                 </div>            
+                <h5 class="city-name"><i class="fa-solid fa-arrow-left"></i> ${item.city}</h5>
+                </div>
+            `
+        document.querySelector(".card-container").innerHTML += card;
+    });
+}
+
+const showCard = () => {
+    //card üzerine tıklayınca
+    document.querySelectorAll(".city-card").forEach((card) => {
+        card.addEventListener("click", () => {
+
+            if (card.classList.contains("active")) {
+                document.querySelector("body").classList.add("body-default-color");
+                card.classList.remove("active")
+            } else {
+                document.querySelectorAll(".city-card").forEach((otherCards) => {
+                    otherCards.classList.remove("active")
+                })
+                card.classList.add("active")
+            }
+        })
+    })
+    //şehir üzerine tıklayınca
+    document.querySelectorAll(".city-name").forEach((city) => {
+        let currentCard = city.previousElementSibling
+        city.addEventListener("click", () => {
+            if (currentCard.classList.contains("active")) {
+                currentCard.classList.remove("active")
+            } else {
+                document.querySelectorAll(".city-card").forEach((otherCards) => {
+                    otherCards.classList.remove("active")
+                })
+                currentCard.classList.add("active")
+            }
+        })
+    })
+}
+
+const setTime = () => {
+    let date = new Date()
+    document.querySelectorAll(".times[id='1']").forEach((city) => {
+        city.innerHTML = `<span class="current-time">${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}<span/>`
+    })
+    document.querySelectorAll(".times[id='2']").forEach((city) => {
+        city.innerHTML = `<span class="current-time">${date.getHours() - 3}:${date.getMinutes()}:${date.getSeconds()}<span/>`
+    })
+    document.querySelectorAll(".times[id='3']").forEach((city) => {
+        city.innerHTML = `<span class="current-time">${date.getHours() - 2}:${date.getMinutes()}:${date.getSeconds()}<span/>`
+    })
+    document.querySelectorAll(".times[id='4']").forEach((city) => {
+        city.innerHTML = `<span class="current-time">${date.getHours() + 5}:${date.getMinutes()}:${date.getSeconds()}<span/>`
+    })
+    document.querySelectorAll(".times[id='5']").forEach((city) => {
+        city.innerHTML = `<span class="current-time">${date.getHours() - 8}:${date.getMinutes()}:${date.getSeconds()}<span/>`
+    })
+    document.querySelectorAll(".times[id='6']").forEach((city) => {
+        city.innerHTML = `<span class="current-time">${date.getHours() + 5}:${date.getMinutes()}:${date.getSeconds()}<span/>`
+    })
+}
+createCard()
+showCard()
+setInterval(setTime, 1000)
+
+
