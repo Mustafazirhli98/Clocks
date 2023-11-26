6e3621f8ea90ee4dfab441ab93f9ddd98c64a555
Mustafa Zırhlı <mustafazirhli98@gmail.com>
2023-11-26 16:30:58 +0300

    proje sonu

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+let container = document.querySelector(".container");
+
+//#region functions 
+//ekran boyutuna göre tasarıma dinamik olarak müdahale edebilmek için.
+const checkScreenWidth = () => {
+    if (window.innerHeight < 900) {
+        container.style.height = "auto";
+        container.style.height = "100%";
+    }
+}
+
 const createCard = () => {
+    //data'nın içindeki her bir eleman için card oluşturur.
     data.map(item => {
         let card = `
                 <div class="card-parent">
-                    <div class="city-card" style ="background-image:url('${item.url}')">
+                    <div id=${item.id} class="city-card" style ="background-image:url('${item.url}')">
                     <div class="times" id=${item.id}>
                     </div>
                  </div>            
-                <h5 class="city-name"><i class="fa-solid fa-arrow-left"></i> ${item.city}</h5>
+                <span class="city-name"><i class="pointerArrow fa-solid fa-arrow-left"></i> ${item.city}</span>
                 </div>
             `
         document.querySelector(".card-container").innerHTML += card;
+
+        //arrow icon'unun mobil boyutta yukarı yöne bakması için
+        let pointerArrow = document.querySelectorAll(".pointerArrow")
+        pointerArrow.forEach((arrow) => {
+            window.addEventListener("resize", () => {
+                let windowWidth = window.innerWidth
+                if (windowWidth < 900) {
+                    arrow.classList.add("fa-rotate-90")
+                } else arrow.classList.remove("fa-rotate-90")
+            })
+        })
+
     });
 }
 
@@ -17,15 +41,23 @@ const showCard = () => {
     //card üzerine tıklayınca
     document.querySelectorAll(".city-card").forEach((card) => {
         card.addEventListener("click", () => {
-
+            let lastColor = `bg-${card.id}`
+            //önceki bg-color'ı kaldırmak için 
+            container.classList.remove(container.classList.item(1))
+            //yeni bg-color eklemek için
+            container.classList.add(lastColor)
             if (card.classList.contains("active")) {
-                document.querySelector("body").classList.add("body-default-color");
-                card.classList.remove("active")
+                card.classList.remove("active");
+                checkScreenWidth()
+                container.classList.remove(container.classList.item(1))
+                container.classList.add("bg-default")
             } else {
                 document.querySelectorAll(".city-card").forEach((otherCards) => {
-                    otherCards.classList.remove("active")
-                })
-                card.classList.add("active")
+                    otherCards.classList.remove("active");
+
+                });
+                card.classList.add("active");
+                checkScreenWidth()
             }
         })
     })
@@ -66,8 +98,9 @@ const setTime = () => {
         city.innerHTML = `<span class="current-time">${date.getHours() + 5}:${date.getMinutes()}:${date.getSeconds()}<span/>`
     })
 }
+//#endregion
 
-//#region funcitons start area
+//#region funcitons start here
 createCard()
 showCard()
 setInterval(setTime, 1000)
